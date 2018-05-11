81c8e8f23178c5752b3f036c231d1bb617641ca6
Sam Wedgwood <sjwedgwood@gmail.com>
2018-05-11 22:45:26 +0100

    Basic Pages (#2)

--- website/__main__.py
+++ website/__main__.py
@@ -1,12 +1,24 @@
 # coding=utf-8
-from flask import Flask
+from flask import Flask, redirect, render_template
+
+from website.constants import DISCORD_INVITE, LOCATIONS
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return f"<h1>Sapscast Test Page</h1>"
+    return render_template("index.html", locations=LOCATIONS)
+
+
+@app.route("/wow")
+def wow():
+    return render_template("wow.html", locations=LOCATIONS)
+
+
+@app.route("/invite")
+def invite():
+    return redirect(DISCORD_INVITE)
 
 
 if __name__ == "__main__":
---
+++ website/constants.py
@@ -0,0 +1,5 @@
+DISCORD_INVITE = "https://discord.gg/VrwSYXu"
+LOCATIONS = {
+    "/": "Home",
+    "/wow": "World of Warcraft"
+}
